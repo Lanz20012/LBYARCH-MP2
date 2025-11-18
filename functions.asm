991e73155babc3e1ebb36c87a889343c4b864ba3
Lanz Wynel Choi <160328136+Lanz20012@users.noreply.github.com>
2025-11-18 22:47:01 +0800

    Add files via upload

---
+++ functions.asm
@@ -0,0 +1,80 @@
+;Factors to compute acceleration (m/s^2)
+;1. Initial Velocity (Vi) - starting speed
+;2. Final Velocity (Vf) - final speed at target distance (1KM)
+;3. Time (t) - duration to reach 1KM
+
+;Formula for acceleration: a = (Vf - Vi) / t
+;Formula to convert KM to M: 1KM = 1000M
+;Formula to convert hours to seconds: 1 hour = 3600 seconds
+
+;Sample Calculation: Given: Vf = 62.5 KM/H, Vi = 0.0 KM/H, t = 10.1 seconds
+;1. Convert Vf to m/s: Vi = 62.5 * (1000 / 3600) = 17.36 m/s
+;2. Calculate acceleration: a = (17.36-0.0) / 10.1 = 1.7188 m/s^2
+;3. Convert to int (1.7188 m/s^2) =  2 m/s^2
+
+global compute_acceleration
+
+;Using the formula (1000/3600) to convert KM/H to M/S
+;We get the constant value of 0.2777777777777778
+section .data
+    convert: dd 0.2777777777777778 ;as float
+
+section .text
+compute_acceleration:
+    ;Inputs: each row indicates the paramter of a car in matrix
+    ;Initial Velocity (Vi) in KM/H, Final Velocity (Vf) in KM/H, Time (t) in seconds
+    ;Outputs: Acceleration (a) in m/s^2 as integer
+
+    ;rdi = float* input (Vi, Vf, t, Vi, Vf, t, ...continued)
+    ;rsi = int* output (a, a, a, ...continued)
+    ;rdx = number of cars (sets of Vi, Vf, t)
+
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    push r13
+
+    mov r12, rdi
+    mov r13, rsi
+    mov rcx, rdx ;counter for number of cars
+
+    movss xmm0, [rel convert] ;load conversion into xmm0
+
+    .loop_all_cars:
+        cmp rcx, 0
+        je .end_all_cars
+
+        ;Load Vi, Vf, t for current car
+        movss xmm1, [r12 + 0] ;Vi
+        movss xmm2, [r12 + 4] ;Vf
+        movss xmm3, [r12 + 8] ;t
+
+        ;change in velocity (Vf - Vi)
+        subss xmm2, xmm1    ;xmm2 = Vf - Vi
+
+        ;Convert change in velocity to m/s
+        mulss xmm2, xmm0    ;xmm2 = (Vf - Vi) * 0.2777777777777778
+
+        ;Calculate acceleration: a = (Vf - Vi) / t
+        divss xmm2, xmm3    ;xmm2 = a in m/s^2
+
+        ;Coversion to integer
+        cvtss2si ebx, xmm2  ;convert float a to int
+
+        ;Store result
+        mov [r13], ebx       ;store acceleration in output
+
+        ;Advance to next car
+        add r12, 12          ;move to next set of (Vi, Vf, t)
+        add r13, 4           ;move to next output position
+
+        dec rcx
+        jmp .loop_all_cars
+
+.end_all_cars:
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    ret
---
+++ main.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Assembly function
+extern void compute_acceleration(float* input, int* output, int n);
+
+int main() {
+    int Y;
+    printf("Enter number of cars (Y): ");
+    scanf("%d", &Y);
+
+    float* input = malloc(Y * 3 * sizeof(float));
+    int* output = malloc(Y * sizeof(int));
+
+    printf("Enter %d rows (Vi,Vf,T in KM/H,KM/H,seconds):\n", Y);
+    for (int i = 0; i < Y; i++) {
+        float vi, vf, t;
+        scanf("%f ,%f ,%f", &vi, &vf, &t);  // taking inputs
+        input[3 * i + 0] = vi;
+        input[3 * i + 1] = vf;
+        input[3 * i + 2] = t;
+    }
+
+    // Assembly call
+    compute_acceleration(input, output, Y);
+
+    // Output results
+    for (int i = 0; i < Y; i++) {
+        printf("%d\n", output[i]);
+    }
+
+    free(input);
+    free(output);
+    return 0;
+}
